2357d245533e6c5edcf68e9afedf58441713cdd5
Jay <jaypatel0915@gmail.com>
2021-02-20 15:11:52 -0500

    first commit

---
+++ Server.py
@@ -0,0 +1,34 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+import TrainedModel as tm
+
+class Server(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        add= '' if self.path=='/styles.css' else '.html'
+        
+        
+        self.path='/pages/'+self.path+ add
+        
+        try:
+            file_to_get=open(self.path[1:]).read()
+            self.send_response(200)
+        except:
+            file_to_get='File not found'
+            self.send_response(404)
+        self.end_headers()
+        self.wfile.write(bytes(file_to_get,'utf-8'))
+    
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        post_data=post_data.decode('utf-8')
+        ans=tm.predict(post_data)
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(bytes(ans, 'utf-8'))
+       
+    
+        
+httpd=HTTPServer(('localhost',8080),Server)
+httpd.serve_forever()
---
+++ TrainedModel.py
@@ -0,0 +1,25 @@
+from PIL import Image
+import os
+import tensorflow as tf
+import numpy as np
+import ast
+from PIL import ImageOps
+import time
+
+path = "training_1"
+s=os.path.dirname(path)
+loaded_model=tf.keras.models.load_model(s)
+
+
+def predict(img):
+    
+    img=ast.literal_eval(img)
+    img=list(img.values())
+    img=np.array(Image.fromarray(np.array(img,dtype='uint8').reshape(200,200,4)).resize((28,28)).convert('L')).reshape(1,28,28,1)/255
+    start=time.time()
+    results = loaded_model.predict(img)
+    return "The number is " + str(np.argmax(results))
+
+
+
+
