6ae571a53a097d16f236721cbfd1b45b58ecbaea
Jay <jaypatel0915@gmail.com>
2021-02-22 12:34:39 -0500

    fixed border issue

--- TrainedModel.py
+++ TrainedModel.py
@@ -5,7 +5,7 @@ import numpy as np
 import ast
 from PIL import ImageOps
 import time
-
+np.set_printoptions(suppress=True)
 path = "training_1"
 s=os.path.dirname(path)
 loaded_model=tf.keras.models.load_model(s)
@@ -18,6 +18,7 @@ def predict(img):
     img=np.array(Image.fromarray(np.array(img,dtype='uint8').reshape(200,200,4)).resize((28,28)).convert('L')).reshape(1,28,28,1)/255
     start=time.time()
     results = loaded_model.predict(img)
+    
     return "The number is " + str(np.argmax(results))
 
 
